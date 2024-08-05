ba901480edaa77db81b13706dee85969ba039b21
Axel Delsol <axel.delsol.pro@gmail.com>
2024-08-05 14:47:36 +0200

    Setup configuration

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Xml.Linq;
+
+
+NameValueCollection config = ConfigurationManager.AppSettings;
+string sAttr = config.Get("token") ?? throw new ArgumentNullException("token key is required");
+
+Console.WriteLine($"The environment variable is {sAttr}");
